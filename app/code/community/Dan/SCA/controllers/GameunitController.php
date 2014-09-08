7c7811f9c2d5869af5c28d5abb75ee7aaa792be1
Dan Shevenell <dan@therenow.co>
2014-09-08 14:44:47 -0500

    add gameunit protection, better product type handling

--- app/code/community/Dan/SCA/controllers/GameunitController.php
+++ app/code/community/Dan/SCA/controllers/GameunitController.php
@@ -20,11 +20,41 @@ class Dan_SCA_GameunitController extends Mage_Core_Controller_Front_Action {
             $this->_redirect('*/*/index');
         } else {
         	$state->load($gameunit->getParentId(), 'entity_id');
-        }
-        
-        Mage::register('current_gameunit', $gameunit);
-        Mage::register('current_state', $state);
+        };
 		
-        $this->loadLayout()->renderLayout();
+		/* handler to prevent showing protected gameunits (and their statistics) */
+		$deny = false;
+		if($gameunit->getMembersOnly()){
+			
+			// get the groupId for 'Members'
+			$targetGroup = Mage::getModel('customer/group');
+			$targetGroup->load('Members', 'customer_group_code');
+			$memberGroupId = $targetGroup->getId();
+			
+			// get the current customer's groupId
+			$customersGroupId = Mage::getSingleton('customer/session')->getCustomerGroupId();
+			
+			// set deny flag if the Id's are not the same
+			if($customersGroupId != $memberGroupId)
+				$deny = true;
+		};
+		
+		if(!$deny){
+	        Mage::register('current_gameunit', $gameunit);
+	        Mage::register('current_state', $state);
+	        $this->loadLayout()->renderLayout();
+		}
+		else{
+			// get url of the membership product and forward the user there
+			$membershipUrl =  Mage::getModel('catalog/product')->loadByAttribute('sku', 'MEMBERSHIP')->getProductUrl();
+			$baseUrl = Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_LINK);
+			$_url = explode($baseUrl, $membershipUrl); 
+			$this->_redirect($_url[1]);
+		};
+
+		/*
+			dan06
+			QatYUJkMC#1
+		*/
     }
 }
